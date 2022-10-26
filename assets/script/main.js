dbd419b912e909f3992108bfe82634f10beb5a4a
Wilian-N-Silva <wilian.silva209@gmail.com>
2022-10-26 16:23:37 -0300

    feat: Internationalized language name

--- assets/script/main.js
+++ assets/script/main.js
@@ -1,18 +1,25 @@
-const defaultLocale = "pt";
-const supportedLocales = ["pt", "es", "en", "fr", "ja"];
+const defaultLocale = "pt"
+const supportedLocales = {
+    "pt": "Português (BR)",
+    "es": "Español",
+    "ja": "日本語",
+    "fr": "Français",
+    "en": "English",
+}
 
 const languageSwitch = document.querySelector("#language-switch")
 
-let translation;
+let translation
 
 document.addEventListener("DOMContentLoaded", () => {
     languageSwitch.replaceChildren("")
-    supportedLocales.forEach((locale) => {
+
+    Object.entries(supportedLocales).forEach(([key, value]) => {
         const option = document.createElement("option")
-        option.value = locale;
-        option.innerText = locale.toUpperCase()
+        option.value = key
+        option.innerText = value
         languageSwitch.append(option)
-    })
+    });
 })
 
 languageSwitch.addEventListener("change", (event) => {
@@ -21,23 +28,23 @@ languageSwitch.addEventListener("change", (event) => {
 
 async function fetchTranslationOf(locale) {
     const langPath = `./assets/lang/${locale}.json`
-    const response = await fetch(langPath);
+    const response = await fetch(langPath)
 
-    return await response.json();
+    return await response.json()
 }
 
 async function changeLocale(newLocale) {
     translation = await fetchTranslationOf(newLocale)
-    translatePage();
+    translatePage()
 }
 
 function translatePage() {
-    const i18nKeys = document.querySelectorAll("[data-i18n-key]");
+    const i18nKeys = document.querySelectorAll("[data-i18n-key]")
 
-    document.querySelector("html").setAttribute("lang", `${translation['language-code']}-${translation['country-code']}`)
+    document.documentElement.lang = `${translation['language-code']}-${translation['country-code']}`
 
     i18nKeys.forEach((element) => {
-        const key = element.getAttribute('data-i18n-key');
+        const key = element.getAttribute('data-i18n-key')
         element.innerHTML = translation['strings'][key]
     })
 }
